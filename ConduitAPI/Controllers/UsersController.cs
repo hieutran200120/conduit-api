9c58947d5626c8dfe7c9190f3961c17b8ed057c8
hieutran200120 <tieucaca097@gmail.com>
2023-09-15 00:13:55 +0700

    put user

--- ConduitAPI/Controllers/UsersController.cs
+++ ConduitAPI/Controllers/UsersController.cs
@@ -1,6 +1,4 @@
-﻿using ConduitAPI.Services.Users;
-using ConduitAPI.Services.Users.Dtos;
-using Microsoft.AspNetCore.Http;
+﻿
 using Microsoft.AspNetCore.Mvc;
 
 namespace ConduitAPI.Controllers
@@ -9,29 +7,5 @@ namespace ConduitAPI.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-        private readonly IUserService _userService;
-        public UsersController(IUserService userService)
-        {
-            _userService = userService;
-        }
-        [HttpPost]
-        public async Task<IActionResult> Register(RegisterRequestDto request)
-        {
-            var res = await _userService.Register(request);
-            return Ok(res);
-        }
-        //TODO: remove username when implement authenticate
-        [HttpGet("{username}")]
-        public async Task<IActionResult> GetCurrentUser(string username)
-        {
-            var res = await _userService.GetCurrentUser(username);
-            return Ok(res);
-        }
-        [HttpPut("{username}")]
-        public async Task<IActionResult> UpdateCurrentUser(string username, UpdateCurrentUserRequestDto request)
-        {
-            var res = await _userService.UpdateCurrentUser(username, request);
-            return Ok(res);
-        }
     }
 }
